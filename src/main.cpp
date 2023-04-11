63eb5f3ceacbbc6cc19a28013b00650fd23852fd
Robert Cobzarencu <101255066+CobzarencuR@users.noreply.github.com>
2023-04-11 18:58:32 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+
+const char *ssid = "Hotspot Name";
+const char *password = "Password";
+
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+
+void initWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval))
+  {
+    Serial.print(millis());
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    previousMillis = currentMillis;
+  }
+}
